8cf255c451f833ee9e5430377288881bd1bc0821
Brian Teague <bpteague@gmail.com>
2015-07-18 22:35:44 -0400

    Get the cytoflow tag for the build tag

--- build_wheel.sh
+++ build_wheel.sh
@@ -1,10 +1,6 @@
 #!/bin/bash
 set -e
 
-#PYTHON_VERSION="2.7"
-#NUMPY_VERSION="1.6"         # 1.6 works for 2.7, 1.7 works for 3.3, 1.8 for 3.4
-SKIMAGE_VERSION="0.10.0"        # version of scikits-image to build wheel for
-
 # Install miniconda 
 # comment this block out for local builds.
 wget http://repo.continuum.io/miniconda/Miniconda-3.5.2-MacOSX-x86_64.sh \
@@ -17,21 +13,16 @@ conda update --yes conda
 # create a conda environment with the necessary Python modules for a build
 conda create --yes -n build_env python=$PYTHON_VERSION
 source activate build_env
-conda install --yes setuptools pip numpy=$NUMPY_VERSION six cython
+conda install --yes setuptools pip numpy=$NUMPY_VERSION six cython pandas numexpr seaborn traits pyface envisage pyqt
 python --version
 
-# Download the scikits-image release files
-mkdir tmp
-cd tmp
-#cp ../v${SKIMAGE_VERSION}.tar.gz .  # testing locally
-wget --no-check-certificate \
-    https://pypi.python.org/packages/source/s/scikit-image/scikit-image-${SKIMAGE_VERSION}.tar.gz \
-    -O v${SKIMAGE_VERSION}.tar.gz
-tar xfz v${SKIMAGE_VERSION}.tar.gz
-cd scikit-image-${SKIMAGE_VERSION}/
+# update the cytoflow submodule
+cd cytoflow
+echo Tag ${TRAVIS_TAG}
+git checkout ${TRAVIS_TAG}
 
 # build the wheel
-echo "Building scikits-image wheel for Python: $PYTHON_VERSION"
+echo "Building cytoflow wheel for Python: $PYTHON_VERSION"
 pip install wheel
 python setup.py bdist_wheel
 
